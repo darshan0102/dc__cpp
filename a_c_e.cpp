1e1515a255b47403bf5a02fa3ee8fcc29d0ef5c6
darshan <darshanchauhan5064@gmail.com>
2023-08-24 17:08:10 +0530

    24aug

--- a_c_e.cpp
+++ a_c_e.cpp
@@ -4,25 +4,24 @@ using namespace std;
 
 int main()
 {
+    int i ,j ;
+    int a = 'A';
 
-  int i;
-
-  for(i = 65; i <90; i+=2)
-  {
- 
-     if(i%4==1)
-     {
-        cout << (char)i <<"\t"; 
-     }
-  
-      else
-
-     {
-        cout << (char)(i + 32)<<"\t";
-
-     }
-
-
-  }
-
+    for ( i = 1; i <= 5; i++)
+    {
+        for ( j = 1; j <= i; j++)
+        {
+            if (i%2==0)
+            {
+                cout << (char )(a + 32) << "\t";
+            }
+            else
+            {
+                cout <<(char)a  << "\t";
+            }
+            a++;
+        }
+        cout << "\n";
+    }
+    
 }
